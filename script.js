beed601b8ac3da8713cfdf03b6816fd5e15cf5f7
Bensaxxy <benjaminsegun38@gmail.com>
2023-07-15 11:28:01 +0100

    update
    update

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// read link
 let click = document.getElementById("read");
 
+// read unseen section
 click.addEventListener("click", () => {
     document.querySelectorAll(".unseen").forEach((e) => {
         e.classList.remove("unseen");
@@ -8,5 +10,6 @@ click.addEventListener("click", () => {
         e.classList.remove("dot");
     });
 
+    // notifcations number
     document.getElementById("num").innerText = "0";
 });
